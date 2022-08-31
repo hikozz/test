6c71a40e5d201f46a1af97f7a8115e36f57e82c8
is7116 <is7116@gmail.com>
2022-08-31 17:19:06 +0600

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,4 +1,4 @@
-SELECT c_Cards.Code as 'Карта', c_Cards.Name as 'ФИО', c_Transactions.Sum as 'Сумма' 
+SELECT c_Cards.Code as 'Карта', c_Cards.Name as 'ФИО', sum(c_Transactions.Sum) as 'Сумма' 
 FROM c_Transactions 
 inner join c_Cards on c_Cards.CardID = c_Transactions.CardID
 WHERE 
@@ -8,4 +8,4 @@ TransactionDateTime <= '2022-08-23 23:59:59.999' AND
 ContractorID = 'ПНТЗ ОАО'
 --AND c_Cards.Code = '0010847470'
 
-GROUP BY c_Cards.Code, c_Cards.Name, c_Transactions.Sum;
+GROUP BY c_Cards.Code, c_Cards.Name;
