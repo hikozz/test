4e40794d334cc3f570d8b52731bf9920cdae8deb
hikozz <112553691+hikozz@users.noreply.github.com>
2022-10-12 10:54:57 +0500

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,8 +1,8 @@
 SELECT 
 	c_Cards.Name as 'ФИО',
---(CONVERT(DATE, c_ProdCategoryCounters.TransactionDateTime)) as 'Дата',
 	SUM(c_ProdCategoryCounters.[Qty]) as 'Кол-во',
 	c_Transactions.SalePlace as 'Столовая'
+	--(CONVERT(DATE, c_ProdCategoryCounters.TransactionDateTime)) as 'Дата'
 FROM c_ProdCategoryCounters
 INNER JOIN c_Cards on c_Cards.CardID = c_ProdCategoryCounters.CardID
 INNER JOIN c_Transactions on c_ProdCategoryCounters.TransactionID = c_Transactions.TransactionID
@@ -12,5 +12,5 @@ AND	c_ProdCategoryCounters.TransactionDateTime BETWEEN '2022-09-01 00:00:00.001'
 AND (c_Transactions.SalePlace = 'Столовая цеха N64' or c_Transactions.SalePlace = 'Буфет АБК')
 AND c_ProdCategoryCounters.CategoryCode = 'Молоко'
 
-GROUP BY c_Cards.Name, c_Transactions.SalePlace;--, c_ProdCategoryCounters.TransactionDateTime;
+GROUP BY c_Cards.Name, c_Transactions.SalePlace; --, c_ProdCategoryCounters.TransactionDateTime;
 ORDER BY c_Cards.Name, c_Transactions.SalePlace;
